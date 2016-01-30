1ad322d3974ff844a6dd019a5f0e0a2386172394
Cameron Stenmark <cameron.stenmark@iss-i.ca>
2016-01-29 16:02:49 -0800

    Feature 3 complete

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -38,4 +38,14 @@ class First extends Application {
 
         $this->render();
     }
+    
+    function zzz() {
+        $this->data['pagebody'] = 'justone';	// this is the view we want shown
+        // pull out the specific quote requested
+        $record = $this->quotes->get(1);
+
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+    }
 }
